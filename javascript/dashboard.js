6177ed3dd74c2952315f9be0597880f0cdf58210
atishay27 <48438744+atishay27@users.noreply.github.com>
2021-03-04 12:37:04 +0530

    Dashboard Page Created

---
+++ javascript/dashboard.js
@@ -0,0 +1,12 @@
+function budget() {
+	document.getElementById("display").src = "budget.html";
+}
+function income() {
+  document.getElementById("display").src = "income.html";
+}
+function expense() {
+  document.getElementById("display").src = "expense.html";
+}
+function savings() {
+  document.getElementById("display").src = "savings.html";
+}
