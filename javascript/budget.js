11545acf035e651aadd52e448759903ffc9aed9e
atishay27 <48438744+atishay27@users.noreply.github.com>
2021-03-05 11:13:47 +0530

    Budget & Savings Panel in Dashboard Implemented

---
+++ javascript/budget.js
@@ -0,0 +1,24 @@
+function display(prog,heading,cost) {
+  content = '<div id="page-wrap">';
+  content = content + '<div class="meter animate orange"><span style="width:' 
+  content= content + prog
+  content = content + '%"><span></span></span></div>';
+  content = content + '</div>';
+  document.getElementById("bar").innerHTML = content;
+  document.getElementById("head").innerHTML = heading;
+  document.getElementById("amt").innerHTML = "Rs "+cost;
+}
+function budget(){
+    display(75,"BUDGET",5000);
+}
+$(".meter > span").each(function () {
+  $(this)
+    .data("origWidth", $(this).width())
+    .width(0)
+    .animate(
+      {
+        width: $(this).data("origWidth")
+      },
+      1200
+    );
+});
