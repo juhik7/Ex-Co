dd9f0c972e0625979d017b3d1ebebd752e03cfb3
Tushar Ankit <tusshar2ankit@gmail.com>
2021-03-10 14:30:44 +0530

    DataFeed Page Improved

---
+++ javascript/dataFeed.js
@@ -0,0 +1,4 @@
+function getDate(){
+    var today = new Date();
+    document.getElementById('date').value = today.getFullYear() + '-' + ('0' + (today.getMonth() + 1)).slice(-2) + '-' + ('0' + today.getDate()).slice(-2);
+}
