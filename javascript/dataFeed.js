c2e4d2569a266d62b83be06f759c472a58d4d862
atishay27 <48438744+atishay27@users.noreply.github.com>
2021-03-10 20:06:36 +0530

    javascript deleted

--- javascript/dataFeed.js
+++
@@ -1,4 +0,0 @@
-function getDate(){
-    var today = new Date();
-    document.getElementById('date').value = today.getFullYear() + '-' + ('0' + (today.getMonth() + 1)).slice(-2) + '-' + ('0' + today.getDate()).slice(-2);
-}
