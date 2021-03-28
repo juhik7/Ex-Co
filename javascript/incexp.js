7032a2807eaf050945047611a398e9b67aa75a04
atishay27 <48438744+atishay27@users.noreply.github.com>
2021-03-28 14:16:41 +0530

    FULL FUNCTIONALITY EDITED
    This is a major update in the project where we merged pages and improved the overall look and working of our website

--- javascript/incexp.js
+++ javascript/incexp.js
@@ -9,3 +9,57 @@ signUpButton.addEventListener('click', () => {
 signInButton.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
 });
+
+function checkINCOME(){
+	var incField = document.getElementById("income");
+	if(incField.value >= 1){
+		incField.setCustomValidity("");
+	}else{
+		incField.setCustomValidity("Income Can't be below 1");
+	}
+}
+
+function checkIDATE(){
+	var dateField = document.getElementById("idate");
+	var splitDate = dateField.value.split("-");
+	var cdate = new Date();
+	var cmonth = cdate.getMonth() + 1;
+	var cyear = cdate.getFullYear();
+	var imonth = parseInt(splitDate[1]);
+	var iyear = parseInt(splitDate[0]);
+	if(imonth==cmonth && iyear==cyear){
+		dateField.setCustomValidity("");
+	}else{
+		dateField.setCustomValidity("You can enter Income for current month only");
+	}
+}
+
+function checkEDATE(){
+	var dateField = document.getElementById("edate");
+	var splitDate = dateField.value.split("-");
+	var cdate = new Date();
+	var cmonth = cdate.getMonth() + 1;
+	var cyear = cdate.getFullYear();
+	var emonth = parseInt(splitDate[1]);
+	var eyear = parseInt(splitDate[0]);
+	if(emonth==cmonth && eyear==cyear){
+		dateField.setCustomValidity("");
+	}else{
+		dateField.setCustomValidity("You can enter Expense for current month only");
+	}
+}
+
+function checkEXPENSE(){
+	var expField = document.getElementById("expense");
+	if(expField.value >= 1){
+		expField.setCustomValidity("");
+	}else{
+		expField.setCustomValidity("Expense Can't be below 1");
+	}
+}
+window.onload = function () {
+    document.getElementById("income").oninput = checkINCOME;
+    document.getElementById("expense").oninput = checkEXPENSE;
+    document.getElementById("idate").oninput = checkIDATE;
+    document.getElementById("edate").oninput = checkEDATE;
+}
--- javascript/login.js
+++ javascript/login.js
@@ -9,3 +9,42 @@ signUpButton.addEventListener('click', () => {
 signInButton.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
 });
+
+
+function checkAGE(){
+	var ageField=document.getElementById("age");
+	var age = ageField.value;
+	if (age>=13) {
+		ageField.setCustomValidity("");
+	}else if (age>=1) {
+		ageField.setCustomValidity("User should be above 13 years of age");
+	}else{
+		ageField.setCustomValidity("Please fill out this field");
+	}
+
+}
+
+function checkPASS(){
+	var passw = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
+	var passField = document.getElementById("rpass");
+	if(passField.value.match(passw)){
+		passField.setCustomValidity("");
+	}else{
+		passField.setCustomValidity("Password must be between 6 to 20 characters which contain at least one numeric digit, one uppercase and one lowercase letter");
+	}
+}
+
+function checkCPASS(){
+	var passField = document.getElementById("rpass");
+	var passCField = document.getElementById("rcpass");
+	if(passField.value == passCField.value){
+		passCField.setCustomValidity("");
+	}else{
+		passCField.setCustomValidity("Confirm Password & Password not matching");
+	}
+}
+window.onload = function () {
+    document.getElementById("age").oninput = checkAGE;
+    document.getElementById("rpass").oninput = checkPASS;
+    document.getElementById("rcpass").oninput = checkCPASS;
+}
